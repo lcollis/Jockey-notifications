dadc5080bac43beddd07bf981724d44a5bbe42f2
Matthew Caswell <mattpcaswell@gmail.com>
2016-12-20 18:17:30 -0500

    made some changes

--- notificationController.js
+++ notificationController.js
@@ -8,33 +8,52 @@ var fcm = new FCM(serverKey);
 
 
 firebase.initializeApp({
-  serviceAccount: "../Bespoke Jockey-e3ea1842d9dc.json",
-  databaseURL: "https://databaseName.firebaseio.com"
+  serviceAccount: "./Bespoke Jockey-5496b7ff6456.json",
+  databaseURL: "https://bespoke-jockey.firebaseio.com/"
 });
 
 var db = firebase.database();
 var chatsRef = db.ref("chats");
+var pushTokensRef = db.ref("pushTokens");
 
 chatsRef.on("child_changed", function(snapshot) {
     var changedChat = snapshot.val();
+    console.log("changed chat: " + JSON.stringify(changedChat));
+    sendNewMessagePushNotification(changedChat.default.guestID);
 });
 
-function pushNewMessageNotification(pushUserID, messageText) {
-
+function sendNewMessagePushNotification(userID, messageText) {
     var message = {
-        'to': pushUserID,
         collapse_key: 'new message',
         notification: {
             title: "New Message From the Front Desk",
-            body: messageText
+            body: "You have a new message from the front desk"
         }
     };
+
+    getUserPushToken(userID, function(pushToken) {
+        sendPushNotification(pushToken, message);
+    });
+}
+
+function getUserPushToken(userID, callback) {
+    db.ref("pushTokens/" + userID).once("value").then(function(snapshot) {
+        callback(snapshot.val());
+    })
+    .catch(function(error) {
+        console.log("got error getting push token");
+        console.log(error);
+    });
+}
+
+function sendPushNotification(pushToken, message) {
+    message.to = pushToken;
 
     fcm.send(message)
         .then(function (response) {
             console.log("Successfully sent with response: " + response);
         })
         .catch(function (error) {
-            console.log("Got error: " + eror);
+            console.log("Got error: " + error);
         });
 }
